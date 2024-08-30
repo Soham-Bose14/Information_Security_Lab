46fca2dc4f960c9d2f5d142be85dc5ced2ace505
Soham-Bose14 <147504424+Soham-Bose14@users.noreply.github.com>
2024-08-30 09:50:24 +0530

    Update affineCipher.py

--- Lab1/affineCipher.py
+++ Lab1/affineCipher.py
@@ -1,124 +1,99 @@
-
 def additive_cipher(msg, key):
-    pos = 0
-    new_pos = 0
-    start = 0
     encrypted_msg = ""
     for ch in msg:
-        if(ch.isalpha()==False):
+        if not ch.isalpha():
+            encrypted_msg += ch
             continue
-        if(ch.isupper()==True):
-            start = 65
-
-        elif(ch.islower()==True):
-            start = 97
+        start = 65 if ch.isupper() else 97
         pos = ord(ch) - start
         new_pos = (pos + key) % 26
         encrypted_msg += chr(new_pos + start)
     return encrypted_msg
 
 def decrypt_additive_cipher(msg, key):
-    pos = 0
-    new_pos = 0
-    start = 0
     decrypted_msg = ""
     for ch in msg:
-        if(ch.isalpha()==False):
+        if not ch.isalpha():
+            decrypted_msg += ch
             continue
-        if(ch.isupper()==True):
-            start = 65
-        elif(ch.islower()==False):
-            start = 97
+        start = 65 if ch.isupper() else 97
         pos = ord(ch) - start
-        new_pos = (pos+26-key)%26
-        decrypted_msg += chr(new_pos+start)
+        new_pos = (pos - key) % 26
+        decrypted_msg += chr(new_pos + start)
     return decrypted_msg
 
 def multiplicative_cipher(msg, key):
-    new_pos = 0
-    start = 0
     encrypted_msg = ""
     for ch in msg:
-        if (ch.isalpha() == False):
+        if not ch.isalpha():
+            encrypted_msg += ch
             continue
-        if (ch.isupper() == True):
-            start = 65
-
-        elif (ch.islower() == True):
-            start = 97
+        start = 65 if ch.isupper() else 97
         pos = ord(ch) - start
         new_pos = (pos * key) % 26
         encrypted_msg += chr(new_pos + start)
     return encrypted_msg
 
-def decrypt_multiplicative_cipher(msg, key):
-    pos = 0
-    new_pos = 0
-    start = 0
-    decrypted_msg = ""
-    for ch in msg:
-        if(ch.isalpha()==False):
-            continue
-        if(ch.isupper()==True):
-            start = 65
-        elif(ch.islower()==False):
-            start = 97
-        pos = ord(ch) - start
-        new_pos = (pos*(26-key)) % 26
-        decrypted_msg += chr(new_pos+start)
-    return decrypted_msg
-
 def modular_inverse(num, m):
     for x in range(1, m):
-        if ((num % m) * (x % m)) % m == 1:
+        if (num * x) % m == 1:
             return x
     return 0
 
+def decrypt_multiplicative_cipher(msg, key):
+    decrypted_msg = ""
+    a_inverse = modular_inverse(key, 26)
+    if a_inverse == 0:
+        raise ValueError("No modular inverse exists for the given key.")
+    for ch in msg:
+        if not ch.isalpha():
+            decrypted_msg += ch
+            continue
+        start = 65 if ch.isupper() else 97
+        pos = ord(ch) - start
+        new_pos = (pos * a_inverse) % 26
+        decrypted_msg += chr(new_pos + start)
+    return decrypted_msg
+
 def affine_cipher(msg, key):
-    new_pos = 0
-    start = 0
     encrypted_msg = ""
     for ch in msg:
-        if (ch.isalpha() == False):
+        if not ch.isalpha():
+            encrypted_msg += ch
             continue
-        if (ch.isupper() == True):
-            start = 65
-
-        elif (ch.islower() == True):
-            start = 97
+        start = 65 if ch.isupper() else 97
         pos = ord(ch) - start
         new_pos = (pos * key[0] + key[1]) % 26
         encrypted_msg += chr(new_pos + start)
     return encrypted_msg
 
 def decrypt_affine_cipher(msg, key):
-    pos = 0
-    new_pos = 0
-    start = 0
     decrypted_msg = ""
+    a_inverse = modular_inverse(key[0], 26)
+    if a_inverse == 0:
+        raise ValueError("No modular inverse exists for the given key.")
     for ch in msg:
-        if(ch.isalpha()==False):
+        if not ch.isalpha():
+            decrypted_msg += ch
             continue
-        if(ch.isupper()==True):
-            start = 65
-        elif(ch.islower()==False):
-            start = 97
-
+        start = 65 if ch.isupper() else 97
         pos = ord(ch) - start
-        a_inverse = modular_inverse(pos, 26)
-        new_pos = (a_inverse*(pos-key[1])) % 26
-        decrypted_msg += chr(new_pos+start)
+        new_pos = (a_inverse * (pos - key[1])) % 26
+        decrypted_msg += chr(new_pos + start)
     return decrypted_msg
-
 
 msg = "I am learning information security"
 print(f'Message to be encrypted: {msg}\n')
+
 key = 20
 print(f'Encrypted message using additive cipher: {additive_cipher(msg, key)}')
+
 key = 15
 print(f'Encrypted message using multiplicative cipher: {multiplicative_cipher(msg, key)}')
+
 affine_key = (15, 20)
 print(f'Encrypted message using affine cipher: {affine_cipher(msg, affine_key)}\n')
+
 print(f'Decrypted message using additive cipher: {decrypt_additive_cipher(additive_cipher(msg, key), key)}')
 print(f'Decrypted message using multiplicative cipher: {decrypt_multiplicative_cipher(multiplicative_cipher(msg, key), key)}')
 print(f'Decrypted message using affine cipher: {decrypt_affine_cipher(affine_cipher(msg, affine_key), affine_key)}\n')
